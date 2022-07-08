bdd6449d6f5362e004c005c0e8af92b7ca8443f3
Georg Semmler <github@weiznich.de>
2022-07-08 19:00:33 +0200

    Add a first axum test-case based on #2
    Closes #2

--- test_cases/tests/lib.rs
+++ test_cases/tests/lib.rs
@@ -18,3 +18,9 @@ fn uom() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/uom/*.rs");
 }
+
+#[test]
+fn axum() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/axum/*.rs");
+}
